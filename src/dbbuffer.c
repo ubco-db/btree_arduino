e9f4e495fc443269748d19b98e0c90a538d93267
Ramon Lawrence <ramon.lawrence@ubc.ca>
2021-03-16 15:24:25 -0700

    Arduino version

---
+++ src/dbbuffer.c
@@ -0,0 +1,383 @@
+/******************************************************************************/
+/**
+@file		dbbuffer.c
+@author		Ramon Lawrence
+@brief		Light-weight buffer implementation for small embedded devices.
+@copyright	Copyright 2021
+			The University of British Columbia,		
+@par Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+@par 1.Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+@par 2.Redistributions in binary form must reproduce the above copyright notice,
+	this list of conditions and the following disclaimer in the documentation
+	and/or other materials provided with the distribution.
+
+@par 3.Neither the name of the copyright holder nor the names of its contributors
+	may be used to endorse or promote products derived from this software without
+	specific prior written permission.
+
+@par THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+/******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+
+#include "dbbuffer.h"
+#include "btree.h"
+
+/**
+@brief     	Initializes buffer given page size and number of pages.
+@param     	state
+                DBbuffer state structure
+*/
+void dbbufferInit(dbbuffer *state)
+{
+	printf("Initializing buffer.\n");
+	printf("Buffer size: %d  Page size: %d\n", state->numPages, state->pageSize);			
+	
+	/* TODO: These values would be set during recovery if database already exists. */
+	state->nextPageId = 0;
+	state->nextPageWriteId = 0;		
+
+	state->numReads = 0;
+	state->numWrites = 0;
+	state->numOverWrites = 0;
+	state->bufferHits = 0;
+	state->lastHit = 0;
+	state->nextBufferPage = 1;
+	
+	/* Clear buffer status flags */
+	for (count_t l=0; l < state->numPages; l++)
+		state->status[l] = 0;	
+}
+
+/**
+@brief      Reads page to a particular buffer number. Returns pointer to buffer if success.
+@param     	state
+                DBbuffer state structure
+@param     	pageNum
+                Physical page id (number)
+@param		bufferNum
+				Buffer to read into
+@return		Returns pointer to buffer page or NULL if error.
+*/
+void* readPageBufferInternal(dbbuffer *state, id_t pageNum, count_t bufferNum)
+{
+	void *buf = state->buffer + bufferNum * state->pageSize;	
+	ION_FILE* fp = state->file;
+  
+    /* Seek to page location in file */
+    fseek(fp, pageNum*state->pageSize, SEEK_SET);
+
+    /* Read page into start of buffer 1 */   
+    if (0 ==  fread(buf, state->pageSize, 1, fp))
+    	return NULL;       
+    
+    state->numReads++;
+	   
+	return buf;
+}
+
+/**
+@brief      Reads page to a particular buffer number. Returns pointer to buffer if success.
+@param     	state
+                DBbuffer state structure
+@param     	pageNum
+                Physical page id (number)
+@param		bufferNum
+				Buffer to read into
+@return		Returns pointer to buffer page or NULL if error.
+*/
+void* readPageBuffer(dbbuffer *state, id_t pageNum, count_t bufferNum)
+{
+	/* Check to see if page is currently in buffer */
+	for (count_t i=1; i < state->numPages; i++)
+	{
+		if (state->status[i] == pageNum && pageNum != 0)
+		{
+			state->bufferHits++;
+			void* buf = state->buffer + state->pageSize*i;
+			state->lastHit = state->status[i];
+			if (i != bufferNum)
+			{	memcpy(state->buffer + bufferNum*state->pageSize, buf, state->pageSize);
+				return state->buffer + bufferNum*state->pageSize;
+			}
+			return buf;
+		}
+	}
+	return readPageBufferInternal(state, pageNum, bufferNum);	
+}
+
+
+/**
+@brief      Reads page either from buffer or from storage. Returns pointer to buffer if success.
+@param     	state
+                DBbuffer state structure
+@param     	pageNum
+                Physical page id (number)
+@return		Returns pointer to buffer page or NULL if error.
+*/
+void* readPage(dbbuffer *state, id_t pageNum)
+{    
+	void *buf;
+	count_t i;	
+
+	/* Check to see if page is currently in buffer */
+	for (i=1; i < state->numPages; i++)
+	{
+		if (state->status[i] == pageNum && pageNum != 0)
+		{
+			state->bufferHits++;
+			buf = state->buffer + state->pageSize*i;
+			state->lastHit = state->status[i];
+			return buf;
+		}
+	}
+
+	if (state->numPages == 2)
+	{	buf = state->buffer + state->pageSize;
+		i = 1;
+	}
+	else
+	{	
+		/* Reserve page #1 for root if have at least 3 buffers. */
+		if (state->activePath[0] == pageNum)
+		{	/* Request for root. */			
+			i = 1;
+		}
+		else
+		{
+			if (state->numPages == 3)
+			{	/* With 3 pages and not the root, always reusing the 3rd buffer for reading. */
+				buf = state->buffer + state->pageSize*2;
+				i = 2;
+			}
+			else
+			{
+				/* More than minimum pages. Some basic memory management using round robin buffer. */		
+				buf = NULL;
+				/* Determine buffer location for page */
+				/* TODO: This needs to be improved and may also consider locking pages */
+				for (i=2; i < state->numPages; i++)
+				{
+					if (state->status[i] == 0)	/* Empty page */
+					{	buf = state->buffer + state->pageSize*i;			
+						break;
+					}
+				}
+
+				/* Pick the next page */
+				if (buf == NULL)
+				{
+					i = state->nextBufferPage;
+					state->nextBufferPage++;
+					while (1)
+					{
+						if (i > state->numPages-1)
+						{	i = 2;
+							state->nextBufferPage = 2;
+						}
+
+						if (state->status[i] != state->lastHit)						
+							break;					
+
+						i++;
+					}		
+				}
+			}
+		}
+	}
+	    
+	state->status[i] = pageNum;
+	return readPageBufferInternal(state, pageNum, i);
+}
+
+
+
+/**
+@brief      Writes page to storage. Returns physical page id if success. -1 if failure.
+			This version does not check for wrap around.
+@param     	state
+               	DBbuffer state structure
+@param     	buffer
+                In memory buffer containing page
+@param		pageNum
+				Location to write at
+@return		
+*/
+int32_t writeBytes(dbbuffer *state, void* buffer, count_t size, int32_t pageNum, int32_t offset)
+{			
+	/* Seek to page location in file */
+    fseek(state->file, pageNum*state->pageSize+offset, SEEK_SET);
+
+	fwrite(buffer, size, 1, state->file);
+	#ifdef DEBUG_WRITE
+            printf("Wrote block. Idx: %d Cnt: %d\n", *((int32_t*) buffer), SBTREE_GET_COUNT(state->buffer));
+			printf("BM: "BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY( *((uint8_t*) (state->buffer+state->bmOffset))));
+            for (int k = 0; k < SBTREE_GET_COUNT(buffer); k++)
+            {
+                test_record_t *buf = (void *)(buffer + state->headerSize + k * state->recordSize);
+                printf("%d: Output Record: %d\n", k, buf->key);
+            }
+	#endif
+
+	// state->numWrites++;		
+	
+	// printf("\nWrite bytes page: %d Id: %d Key: %d\n", pageNum, (state->nextPageId-1), *((int32_t*) (buffer+10)));
+	return pageNum;
+}
+
+/**
+@brief      Writes page to storage. Returns physical page id if success. -1 if failure.
+			This version does not check for wrap around.
+@param     	state
+               	DBbuffer state structure
+@param     	buffer
+                In memory buffer containing page
+@param		pageNum
+				Location to write at
+@return		
+*/
+int32_t writePageDirect(dbbuffer *state, void* buffer, int32_t pageNum)
+{
+	/* Setup page number in header */	
+	memcpy(buffer, &(state->nextPageId), sizeof(id_t));
+	state->nextPageId++;
+		
+	/* Seek to page location in file */
+    fseek(state->file, pageNum*state->pageSize, SEEK_SET);
+
+	fwrite(buffer, state->pageSize, 1, state->file);
+	#ifdef DEBUG_WRITE
+            printf("Wrote block. Idx: %d Cnt: %d\n", *((int32_t*) buffer), SBTREE_GET_COUNT(state->buffer));
+			printf("BM: "BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY( *((uint8_t*) (state->buffer+state->bmOffset))));
+            for (int k = 0; k < SBTREE_GET_COUNT(buffer); k++)
+            {
+                test_record_t *buf = (void *)(buffer + state->headerSize + k * state->recordSize);
+                printf("%d: Output Record: %d\n", k, buf->key);
+            }
+	#endif
+
+	state->numWrites++;		
+	
+	// printf("\nWrite page: %d Id: %d Key: %d\n", pageNum, (state->nextPageId-1), *((int32_t*) (buffer+10)));
+	return pageNum;
+}
+
+/**
+@brief      Overwrites page to storage at same physical address. -1 if failure.
+			Caller is responsible for knowing that overwrite is possible given page contents.
+@param     	state
+                DBbuffer state structure
+@param     	buffer
+                In memory buffer containing page
+@return		
+*/
+int32_t overWritePage(dbbuffer *state, void* buffer, int32_t pageNum)
+{			
+	/* Seek to page location in file */
+    fseek(state->file, pageNum*state->pageSize, SEEK_SET);
+
+	fwrite(buffer, state->pageSize, 1, state->file);
+	#ifdef DEBUG_WRITE
+            printf("Wrote block. Idx: %d Cnt: %d\n", *((int32_t*) buffer), SBTREE_GET_COUNT(state->buffer));
+			printf("BM: "BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY( *((uint8_t*) (state->buffer+state->bmOffset))));
+            for (int k = 0; k < SBTREE_GET_COUNT(buffer); k++)
+            {
+                test_record_t *buf = (void *)(buffer + state->headerSize + k * state->recordSize);
+                printf("%d: Output Record: %d\n", k, buf->key);
+            }
+	#endif
+
+	state->numOverWrites++;		
+	
+	/* Check if buffer contains this page */
+	for (count_t i=1; i < state->numPages; i++)
+	{		
+		if (state->status[i] == pageNum && pageNum != 0)
+		{	/* Copy over page */
+			memcpy(state->buffer + i*state->pageSize, buffer, state->pageSize);
+			// Other choice is to clear the buffer
+			// state->status[i] = 0;			
+		}
+	}
+
+	// printf("\nWrite page: %d Id: %d Key: %d\n", pageNum, (state->nextPageId-1), *((int32_t*) (buffer+10)));
+	return pageNum;
+}
+
+/**
+@brief      Writes page to storage. Returns physical page id if success. -1 if failure.
+@param     	state
+               	DBbuffer state structure
+@param     	buffer
+                In memory buffer containing page
+@return		
+*/
+int32_t writePage(dbbuffer *state, void* buffer)
+{    		
+	int32_t pageNum;
+	
+	/* TODO: Handle when get to end of file? */
+	pageNum = state->nextPageWriteId++;
+	return writePageDirect(state, buffer, pageNum);	
+}
+
+
+/**
+@brief     	Initialize in-memory buffer page.
+@param     	state
+                DBbuffer state structure
+@param     	pageNum
+                In memory buffer page id (number)
+@return		pointer to initialized page
+*/
+void* initBufferPage(dbbuffer *state, int pageNum)
+{	
+	/* Insure all values are 0 in page. */
+	/* TODO: May want to initialize to all 1s for certain memory types. */	
+	void *buf = state->buffer + pageNum * state->pageSize;
+	for (uint16_t i = 0; i < state->pageSize/sizeof(int32_t); i++)
+    {
+        ((int32_t*) buf)[i] = 0;
+    }
+	
+	return buf;		
+}
+
+/**
+@brief     	Closes buffer.
+@param     	state
+                DBbuffer state structure
+*/
+void closeBuffer(dbbuffer *state)
+{
+	printStats(state);	
+	fclose(state->file);
+}
+
+/**
+@brief     	Prints statistics.
+@param     	state
+                DBbuffer state structure
+*/
+void printStats(dbbuffer *state)
+{
+	printf("Num reads: %lu\n", state->numReads);
+	printf("Buffer hits: %lu\n", state->bufferHits);
+	printf("Num writes: %lu\n", state->numWrites);
+	printf("Num overwrites: %lu\n", state->numOverWrites);
+}
---
+++ src/test_btree.h
@@ -0,0 +1,297 @@
+/******************************************************************************/
+/**
+@file		test_btree.h
+@author		Ramon Lawrence
+@brief		This file does performance/correctness testing of BTree.
+@copyright	Copyright 2021
+			The University of British Columbia,		
+@par Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+
+@par 1.Redistributions of source code must retain the above copyright notice,
+	this list of conditions and the following disclaimer.
+
+@par 2.Redistributions in binary form must reproduce the above copyright notice,
+	this list of conditions and the following disclaimer in the documentation
+	and/or other materials provided with the distribution.
+
+@par 3.Neither the name of the copyright holder nor the names of its contributors
+	may be used to endorse or promote products derived from this software without
+	specific prior written permission.
+
+@par THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+	POSSIBILITY OF SUCH DAMAGE.
+*/
+/******************************************************************************/
+#include <time.h>
+#include <string.h>
+
+#include "btree.h"
+#include "randomseq.h"
+
+
+int32_t checkValues(btreeState *state, void* recordBuffer, int32_t* vals, uint32_t n)
+{
+     /* Verify that can find all values inserted */
+    uint32_t errors = 0;
+    for (uint32_t i = 0; i < n; i++) 
+    { 
+        int32_t key = vals[i];        
+        int8_t result = btreeGet(state, &key, recordBuffer);
+        if (result != 0) 
+        {   errors++;
+            printf("ERROR: Failed to find: %d\n", key);
+            btreeGet(state, &key, recordBuffer);
+        }
+        else if (*((int32_t*) recordBuffer) != key)
+        {   printf("ERROR: Wrong data for: %d\n", key);
+            printf("Key: %d Data: %d\n", key, *((int32_t*) recordBuffer));
+        }
+    }
+    return errors;
+}
+
+/**
+ * Runs all tests and collects benchmarks
+ */ 
+void runalltests_btree()
+{    
+    uint32_t errors = 0;
+    uint32_t i, j;
+
+    srand(1);
+    randomseqState rnd;
+    rnd.size = 1000;
+    uint32_t n = 1000; 
+    rnd.prime = 0;
+
+        
+    randomseqInit(&rnd);
+    uint32_t *vals = (uint32_t*) malloc(n * sizeof(uint32_t));
+
+    for (uint32_t i=0; i < n; i++)
+    {
+        vals[i] = randomseqNext(&rnd);
+        // printf("%d\n", vals[i]);
+        // See if number exists 
+        for (j=0; j < i; j++)
+            if (vals[j] == vals[i])
+            {   printf("Error: Number generated: %d\n", vals[j]);
+           //     return;
+            }
+    }
+
+    // Check if can regenerate sequence 
+    srand(1);
+    randomseqInit(&rnd);
+    for (i=0; i < n; i++)
+    {
+        uint32_t tmp = randomseqNext(&rnd);
+       //  printf("%d %d\n", tmp, vals[i]);
+        if (tmp != vals[i])
+            printf("ERROR with sequence.\n");
+    }
+    
+
+   
+    int8_t M = 3;        
+   
+    /* Configure buffer */
+    dbbuffer* buffer = (dbbuffer*) malloc(sizeof(dbbuffer));
+    buffer->pageSize = 512;
+    buffer->numPages = M;
+    buffer->status = (id_t*) malloc(sizeof(id_t)*M);
+    buffer->buffer  = malloc((size_t) buffer->numPages * buffer->pageSize);   
+   
+    /* Configure btree state */
+    btreeState* state = (btreeState*) malloc(sizeof(btreeState));
+
+    state->recordSize = 16;
+    state->keySize = 4;
+    state->dataSize = 12;       
+
+    /* Connections betwen buffer and btree */
+    buffer->activePath = state->activePath;
+    buffer->state = state;    
+
+    state->tempKey = malloc(sizeof(int32_t)); 
+    state->tempData = malloc(12); 
+    int8_t* recordBuffer = (int8_t*) malloc(state->recordSize);
+
+    state->mappingBufferSize = 5000;
+    state->mappingBuffer = malloc(state->mappingBufferSize);	
+
+    /* Setup output file. TODO: Will replace with direct memory access. */
+    ION_FILE *fp;
+    fp = fopen("myfile.bin", "w+b");
+    if (NULL == fp) {
+        printf("Error: Can't open file!\n");
+        return;
+    }
+    
+    buffer->file = fp;
+
+    state->parameters = 0;    
+    state->buffer = buffer;
+
+    /* Initialize btree structure with parameters */
+    btreeInit(state);
+
+    /* Data record is empty. Only need to reset to 0 once as reusing struct. */        
+    for (i = 0; i < (uint16_t) (state->recordSize-4); i++) // 4 is the size of the key
+    {
+        recordBuffer[i + sizeof(int32_t)] = 0;
+    }
+
+    unsigned long start = millis();   
+
+    srand(1);
+    randomseqInit(&rnd);
+
+    for (i = 0; i < n ; i++)
+    {   
+        id_t v = randomseqNext(&rnd);// vals[i]; 
+        
+       // printf("\n****STARTING KEY: %d\n",v);
+        // btreePrint(state);    
+/*
+        if (v == 375 || v == 151)       
+        {
+            printf("KEY: %d\n",v);
+            btreePrint(state);    
+            btreePrintMappings(state);       
+        }
+    */
+       // printf("KEY: %d\n",v);
+        *((int32_t*) recordBuffer) = v;
+        *((int32_t*) (recordBuffer+4)) = v;             
+
+        if (btreePut(state, recordBuffer, (void*) (recordBuffer + 4)) == -1)
+        {  
+            btreePrint(state);               
+            printf("INSERT ERROR: %d\n", v);
+            return;
+        }
+        /*
+        if (v == 204 || v == 151)       
+        {
+            printf("KEY: %d\n",v);
+            btreePrint(state);  
+            btreePrintMappings(state);         
+        }
+        */
+       /*
+        if (checkValues(state, recordBuffer, vals, i) > 0)
+        {   printf("Error finding value. Key: %d\n", v);
+            btreePrint(state);   
+            return;
+        }
+        */
+        if (i % 10000 == 0)
+        {           
+            printf("Num: %d KEY: %d\n", i, v);
+            //btreePrint(state);               
+        }        
+    }    
+
+    unsigned long end = millis();   
+
+    // btreePrint(state);     
+    
+    printStats(state->buffer);
+
+    printf("Elapsed Time: %lu s\n", (end - start));
+    printf("Records inserted: %d\n", n);
+
+    printf("\nVerifying and searching for all values.\n");
+    start = millis();
+
+    srand(1);
+    randomseqInit(&rnd);
+
+    /* Verify that can find all values inserted */    
+    for (i = 0; i < n; i++) 
+    { 
+        int32_t key = randomseqNext(&rnd);// vals[i];
+        int8_t result = btreeGet(state, &key, recordBuffer);
+        if (result != 0) 
+        {   errors++;
+            printf("ERROR: Failed to find: %d\n", key);
+            btreeGet(state, &key, recordBuffer);
+        }
+        else if (*((int32_t*) recordBuffer) != key)
+        {   printf("ERROR: Wrong data for: %d\n", key);
+            printf("Key: %d Data: %d\n", key, *((int32_t*) recordBuffer));
+        }
+    }
+
+    if (errors > 0)
+        printf("FAILURE: Errors: %d\n", errors);
+    else
+        printf("SUCCESS. All values found!\n");
+    
+    end = millis();
+    printf("Elapsed Time: %lu s\n", (end - start));
+    printf("Records queried: %d\n", n);   
+    printStats(state->buffer);     
+
+    /* Below minimum key search */
+    int32_t key = -1;
+    int8_t result = btreeGet(state, &key, recordBuffer);
+    if (result == 0) 
+        printf("Error1: Key found: %d\n", key);
+
+    /* Above maximum key search */
+    key = 3500000;
+    result = btreeGet(state, &key, recordBuffer);
+    if (result == 0) 
+        printf("Error2: Key found: %d\n", key);
+    
+    free(recordBuffer);
+    
+    btreeIterator it;
+    uint32_t mv = 40;     // For all records, select mv = 1.
+    it.minKey = &mv;
+    int v = 299;
+    it.maxKey = &v;       
+
+    // btreePrint(state); 
+
+    btreeInitIterator(state, &it);
+    i = 0;
+    int8_t success = 1;    
+    uint32_t *itKey, *itData;
+
+    while (btreeNext(state, &it, (void**) &itKey, (void**) &itData))
+    {                      
+        // printf("Key: %d  Data: %d\n", *itKey, *itData);
+        if (i+mv != *itKey)
+        {   success = 0;
+            printf("Key: %d Error\n", *itKey);
+        }
+        i++;        
+    }
+    printf("\nRead records: %d\n", i);
+
+    if (success && i == (v-mv+1))
+        printf("SUCCESS\n");
+    else
+        printf("FAILURE\n");    
+    
+    // printStats(buffer);
+
+    /* Perform various queries to test performance */
+    closeBuffer(buffer);    
+    
+    free(state->buffer->buffer);
+}
+
